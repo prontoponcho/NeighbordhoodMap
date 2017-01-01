a71d38356beec657dde130df14ca9541afa74403
Richard Sipes <rksipes@gmail.com>
2016-12-31 19:21:35 -0800

    feat: made map responsive

--- js/map.js
+++ js/map.js
@@ -2,15 +2,53 @@ var map;
 var lat = 47.6278632;
 var lng = -122.3333217;
 var zoom = 14;
-var mapTypeId = 'terrain'
+var mapTypeId = 'terrain';
+var mobileZoom = 12;
+var desktopZoom = 14;
+
+// center map solution at:
+// http://stackoverflow.com/questions/8792676/center-google-maps-v3-on-browser-resize-responsive
+var center;
+function calculateCenter() {
+    center = map.getCenter();
+};
 
 function initMap() {
     map = new google.maps.Map( document.getElementById('map'), {
         center: {lat: lat, lng: lng},
         zoom: zoom,
-        mapTypeId: mapTypeId
+        disableDefaultUI: true,
+        scrollwheel: false,
+        draggable: true,
+        mapTypeControl: true,
+          mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.RIGHT_BOTTOM
+          },
+          zoomControl: true,
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.RIGHT_CENTER
+          },
+          scaleControl: true,
+          streetViewControl: true,
+          streetViewControlOptions: {
+              position: google.maps.ControlPosition.RIGHT_TOP
+          }
     });
-}
+    google.maps.event.addDomListener(window, 'resize', function() {
+        if (window.innerWidth <= 767) {
+            map.setZoom(mobileZoom);
+        } else {
+            map.setZoom(desktopZoom);
+        }
+        map.setCenter(center);
+    });
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+};
+
+
 
 // var mapAPI = "https://maps.googleapis.com/maps/api/js?";
 // var apiKey = "AIzaSyBPOJXFTbJh9jn8Mg09QENs5BaEXNgDpw0";
