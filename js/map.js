5605098525220cf035d2b563c28190fcf9839fd9
Richard Sipes <rksipes@gmail.com>
2017-01-03 21:40:18 -0800

    refactor: combine map init and marker init

--- js/map.js
+++ js/map.js
@@ -6,6 +6,50 @@ var mapTypeId = 'terrain';
 var mobileZoom = 12;
 var desktopZoom = 14;
 var apiKey = "AIzaSyBPOJXFTbJh9jn8Mg09QENs5BaEXNgDpw0";
+var locations = [
+    {
+        category: "sexual health",
+        name: "Gay City",
+        address: "517 E Pike St, Seattle, WA 98122",
+        coordinates: {lat: 47.6139121, lng: -122.3246327},
+        url: "https://www.gaycity.org/"
+    },
+    {
+        category: "sexual health",
+        name: "Planned Parenthood, Seattle",
+        address: "2001 E Madison St, Seattle, WA 98122",
+        coordinates: {lat: 47.6100869, lng: -122.3221135},
+        url: "https://www.plannedparenthood.org/health-center/washington/seattle/98122/seattle-health-center-3309-91810?utm_campaign=seattle-health-center&utm_medium=organic&utm_source=local-listing"
+    },
+    {
+        category: "sexual health",
+        name: "Planned Parenthood, First Hill",
+        address: "1229 Madison St #1040, Seattle, WA 98104",
+        coordinates: {lat: 47.6145224, lng: -122.3204054},
+        url: "https://www.plannedparenthood.org/health-center/washington/seattle/98104/first-hill-health-center-4069-91810?utm_campaign=first-hill-health-center&utm_medium=organic&utm_source=local-listing"
+    },
+    {
+        category: "urgent care",
+        name: "Group Health, Capitol Hill",
+        address: "201 16th Ave. E. Seattle, WA 98112",
+        coordinates: {lat: 47.6173163, lng: -122.3050968},
+        url: "https://www.ghc.org/html/public/locations/capitol-hill"
+    },
+    {
+        category: "urgent care",
+        name: "ZOOM+ Care, Capitol Hill",
+        address: "531 Broadway E #10, Seattle, WA 98102",
+        coordinates: {lat: 47.6195658, lng: -122.3124495},
+        url: "https://www.zoomcare.com/clinic/zoomcare-capitol-hill"
+    },
+    {
+        category: "urgent care",
+        name: "Immediate Clinic, Capitol Hill",
+        address: "1512 Broadway, Seattle, WA 98122",
+        coordinates: {lat: 47.624008, lng: -122.3214887},
+        url: "https://www.immediateclinic.com/capitol-hill-urgent-care"
+    }
+];
 
 // center map solution at:
 // http://stackoverflow.com/questions/8792676/center-google-maps-v3-on-browser-resize-responsive
@@ -36,6 +80,9 @@ function initMap() {
               position: google.maps.ControlPosition.RIGHT_TOP
           }
     });
+
+    // zoom out for mobile screens, zoom in for larger screens
+    // recenter the map when resizing the window
     google.maps.event.addDomListener(window, 'resize', function() {
         if (window.innerWidth <= 767) {
             map.setZoom(mobileZoom);
@@ -44,25 +91,21 @@ function initMap() {
         }
         map.setCenter(center);
     });
+
     google.maps.event.addDomListener(map, 'idle', function() {
         calculateCenter();
+    });
+
+    // add markers to the map
+    $.each(locations, function(idx, loc) {
+        var marker = new google.maps.Marker({
+            position: loc.coordinates
+        });
+        marker.setMap(map);
     });
 };
 
 
 
-// var mapAPI = "https://maps.googleapis.com/maps/api/js?";
-// var apiKey = "AIzaSyBPOJXFTbJh9jn8Mg09QENs5BaEXNgDpw0";
-// var callback = "initMap";
-// var params = { "key" : apiKey,
-//                "callback" : callback
-//              };
-// mapURL = mapAPI + $.param(params);
 
-// console.log(mapURL);
-
-// $.ajax(mapURL).error( function(e) {
-//     var error = "Could not load the map."
-//     $('map').text(error);
-// });
 
