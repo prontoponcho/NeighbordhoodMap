feb0f0c32c965f230669abd3a30810cc5bcb7df6
Richard Sipes <rksipes@gmail.com>
2017-01-04 14:01:56 -0800

    feat: add list filters

--- js/app.js
+++ js/app.js
@@ -2,21 +2,44 @@ var map = map || {};
 var initLocations = initLocations || {};
 
 var Location = function(data) {
-	this.category = ko.observable(data.category);
-	this.name = ko.observable(data.name);
-	this.address = ko.observable(data.address);
-	this.coordinates = ko.observable(data.coordinates);
-	this.url = ko.observable(data.url);
+    this.category = ko.observable(data.category);
+    this.name = ko.observable(data.name);
+    this.address = ko.observable(data.address);
+    this.coordinates = ko.observable(data.coordinates);
+    this.url = ko.observable(data.url);
+    this.viewable = ko.observable(true);
 };
 
 var ViewModel = function() {
-	var self = this;
+    var self = this;
 
-	this.locations = ko.observableArray( [] );
+    this.locations = ko.observableArray( [] );
 
-	initLocations.forEach( function( loc ) {
-		self.locations.push( new Location( loc ));
-	});
+    initLocations.forEach( function( loc ) {
+        self.locations.push( new Location( loc ));
+    });
+
+    this.filterSexualHealth = function() {
+        self.locations().forEach( function( loc ) {
+            if (loc.category() !== 'sexual health') {
+                console.log(loc.category());
+                loc.viewable(false);
+            } else {
+                loc.viewable(true);
+            }
+        });
+    };
+
+    this.filterUrgentCare = function() {
+        self.locations().forEach( function( loc ) {
+            if (loc.category() !== 'urgent care') {
+                console.log(loc.category());
+                loc.viewable(false);
+            } else {
+                loc.viewable(true);
+            }
+        });
+    };
 
 };
 
