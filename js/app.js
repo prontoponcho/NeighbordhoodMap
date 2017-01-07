9a00a7f7686652797b20ccd7c1100890c3348e84
Richard Sipes <rksipes@gmail.com>
2017-01-06 20:56:54 -0800

    feat: add list data-bind

--- js/app.js
+++ js/app.js
@@ -1,52 +1,145 @@
 var map = map || {};
-var markers = markers || [];
-var infoWindows = infoWindows || [];
-var initLocations = initLocations || [];
 
-var Location = function(data) {
-    this.category = ko.observable(data.category);
-    this.name = ko.observable(data.name);
-    this.address = ko.observable(data.address);
-    this.coordinates = ko.observable(data.coordinates);
-    this.url = ko.observable(data.url);
-    this.viewable = ko.observable(true);
+var initMarker = function( loc, markers ) {
+
+    var marker = new google.maps.Marker( {
+        animation: google.maps.Animation.DROP,
+        position: loc.coordinates,
+        title: loc.name,
+        map: map
+    });
+
+    // info window content
+    var contentString = "<div class='content'>" +
+        "<h4><a target='_blank' href='" + loc.url + "'>" + loc.name + "</a></h4>" +
+        "<p>" + loc.address + "</p>" +
+        "</div>";
+
+    // each marker has infowindow
+    marker.infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    addClickEvents( marker, markers );
+
+    markers.push(marker);
+
+    return marker;
+};
+
+var addClickEvents = function( marker, markers ) {
+
+    // marker animation
+    marker.addListener('click', function( ) {
+
+        // stop all other markers from bouncing
+        // close all other marker infowindows
+        markers.forEach( function ( marker ) {
+            marker.setAnimation(null);
+            marker.infowindow.close();
+        });
+
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        marker.infowindow.open(map, marker);
+
+        google.maps.event.addListener(marker.infowindow, 'closeclick', function() {
+            marker.setAnimation(null);
+        });
+
+        setTimeout(function () { marker.setAnimation(null); }, 2866);
+    });
+};
+
+var Location = function( data, markers ) {
+    this.category = ko.observable( data.category );
+    this.name = ko.observable( data.name );
+    this.address = ko.observable( data.address );
+    this.coordinates = ko.observable( data.coordinates );
+    this.url = ko.observable( data.url );
+    this.viewable = ko.observable( true );
+    this.marker = ko.observable( initMarker( data, markers ) );
 };
 
 var ViewModel = function() {
-    var self = this;
 
-    this.locations = ko.observableArray( [] );
+    var self = this;
+    self.locations = ko.observableArray( [] );
+    self.markers = [];
 
     initLocations.forEach( function( loc ) {
-        self.locations.push( new Location( loc ));
+        var location = new Location( loc, self.markers );
+        self.locations.push( location );
     });
 
-    this.filterSexualHealth = function() {
+    self.filterSexualHealth = function() {
         self.locations().forEach( function( loc ) {
             if (loc.category() !== 'sexual health') {
                 loc.viewable(false);
+                loc.marker().infowindow.close();
+                loc.marker().setMap(null);
             } else {
                 loc.viewable(true);
+                loc.marker().setMap(map);
             }
         });
     };
 
-    this.filterUrgentCare = function() {
+    self.filterUrgentCare = function() {
         self.locations().forEach( function( loc ) {
             if (loc.category() !== 'urgent care') {
                 loc.viewable(false);
+                loc.marker().infowindow.close();
+                loc.marker().setMap(null);
             } else {
                 loc.viewable(true);
+                loc.marker().setMap(map);
             }
         });
     };
 
-    this.filterAllCategories = function() {
+    self.filterAllCategories = function() {
         self.locations().forEach( function ( loc ) {
             loc.viewable(true);
+            loc.marker().infowindow.close();
+            loc.marker().setMap(map);
         })
+    };
+
+    self.animateMarker = function( clickedLocation ) {
+
+        // look into why convenience function doesn't work here.
+        // addClickEvents(clickedLocation.marker(), self.markers);
+
+        var marker = clickedLocation.marker();
+
+        // stop all other markers from bouncing
+        // close all other marker infowindows
+        self.markers.forEach( function ( marker ) {
+            marker.setAnimation(null);
+            marker.infowindow.close();
+        });
+
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+
+        marker.infowindow.open(map, marker);
+
+        google.maps.event.addListener(marker.infowindow, 'closeclick', function() {
+            marker.setAnimation(null);
+        });
+
+        setTimeout(function () { marker.setAnimation(null); }, 2866);
     };
 
 };
 
-ko.applyBindings(new ViewModel());
+var initViewModel = function() {
+
+    initMap();
+
+    ko.applyBindings( new ViewModel() );
+
+};
+
+
+
--- js/map.js
+++ js/map.js
@@ -1,123 +1,74 @@
 var map;
-var markers = markers || [];
-var infoWindows = infoWindows || [];
-var currentMarker;
-var lat = 47.6278632;
-var lng = -122.3333217;
-var zoom = 14;
-var mapTypeId = 'terrain';
 var mobileZoom = 12;
 var desktopZoom = 14;
-var apiKey = "AIzaSyBPOJXFTbJh9jn8Mg09QENs5BaEXNgDpw0";
+
 var initLocations = [
     {
         category: "sexual health",
         name: "Gay City",
         address: "517 E Pike St, Seattle, WA 98122",
         coordinates: {lat: 47.6139121, lng: -122.3246327},
-        url: "https://www.gaycity.org/"
+        url: "https://www.gaycity.org/",
+        marker: null
     },
     {
         category: "sexual health",
         name: "Planned Parenthood, Seattle",
         address: "2001 E Madison St, Seattle, WA 98122",
         coordinates: {lat: 47.6100869, lng: -122.3221135},
-        url: "https://www.plannedparenthood.org/health-center/washington/seattle/98122/seattle-health-center-3309-91810?utm_campaign=seattle-health-center&utm_medium=organic&utm_source=local-listing"
+        url: "https://www.plannedparenthood.org/health-center/washington/seattle/98122/seattle-health-center-3309-91810?utm_campaign=seattle-health-center&utm_medium=organic&utm_source=local-listing",
+        marker: null
     },
     {
         category: "sexual health",
         name: "Planned Parenthood, First Hill",
         address: "1229 Madison St #1040, Seattle, WA 98104",
         coordinates: {lat: 47.6145224, lng: -122.3204054},
-        url: "https://www.plannedparenthood.org/health-center/washington/seattle/98104/first-hill-health-center-4069-91810?utm_campaign=first-hill-health-center&utm_medium=organic&utm_source=local-listing"
+        url: "https://www.plannedparenthood.org/health-center/washington/seattle/98104/first-hill-health-center-4069-91810?utm_campaign=first-hill-health-center&utm_medium=organic&utm_source=local-listing",
+        marker: null
     },
     {
         category: "urgent care",
         name: "Group Health, Capitol Hill",
         address: "201 16th Ave. E. Seattle, WA 98112",
         coordinates: {lat: 47.6173163, lng: -122.3050968},
-        url: "https://www.ghc.org/html/public/locations/capitol-hill"
+        url: "https://www.ghc.org/html/public/locations/capitol-hill",
+        marker: null
     },
     {
         category: "urgent care",
         name: "ZOOM+ Care, Capitol Hill",
         address: "531 Broadway E #10, Seattle, WA 98102",
         coordinates: {lat: 47.6195658, lng: -122.3124495},
-        url: "https://www.zoomcare.com/clinic/zoomcare-capitol-hill"
+        url: "https://www.zoomcare.com/clinic/zoomcare-capitol-hill",
+        marker: null
     },
     {
         category: "urgent care",
         name: "Immediate Clinic, Capitol Hill",
         address: "1512 Broadway, Seattle, WA 98122",
         coordinates: {lat: 47.624008, lng: -122.3214887},
-        url: "https://www.immediateclinic.com/capitol-hill-urgent-care"
+        url: "https://www.immediateclinic.com/capitol-hill-urgent-care",
+        marker: null
     }
 ];
-
-
 
 // center map solution at:
 // http://stackoverflow.com/questions/8792676/center-google-maps-v3-on-browser-resize-responsive
 var center;
-function calculateCenter() {
+var calculateCenter = function() {
     center = map.getCenter();
-};
-
-// add markers, infowindows, & eventListeners to the map
-var addMarkers = function() {
-
-    initLocations.forEach( function( loc ) {
-
-        var marker = new google.maps.Marker( {
-            animation: google.maps.Animation.DROP,
-            position: loc.coordinates,
-            title: loc.name,
-            map: map
-        });
-
-        markers.push(marker);
-
-        // Add info window to marker
-        var contentString = "<div class='content'>" +
-            "<h4><a target='_blank' href='" + loc.url + "'>" + loc.name + "</a></h4>" +
-            "<p>" + loc.address + "</p>" +
-            "</div>";
-
-        marker.infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-
-        // add bouncing marker animation
-        // https://developers.google.com/maps/documentation/javascript/markers
-        marker.addListener('click', function( ) {
-
-            // stop all other markers from bouncing
-            // close all other marker infowindows
-            markers.forEach( function ( marker ) {
-                marker.setAnimation(null);
-                marker.infowindow.close();
-            });
-
-            // set this clicked marker to bounce
-            this.setAnimation(google.maps.Animation.BOUNCE);
-
-            // open infoWindow of this clicked marker
-            this.infowindow.open(map, this);
-
-            // stop marker bouncing when its infowindow is closed
-            google.maps.event.addListener(this.infowindow, 'closeclick', function() {
-                marker.setAnimation(null);
-            });
-
-            setTimeout(function () { marker.setAnimation(null); }, 2866);
-        });
-    });
 };
 
 function initMap() {
 
-    map = new google.maps.Map( document.getElementById('map'), {
-        center: {lat: lat, lng: lng},
+    var zoom = window.innerWidth <= 767 ? mobileZoom : desktopZoom;
+
+    var mapOptions = {
+        center: {
+            lat: 47.6278632,
+            lng: -122.3333217
+        },
         zoom: zoom,
         disableDefaultUI: true,
         scrollwheel: false,
@@ -136,7 +87,9 @@ function initMap() {
         streetViewControlOptions: {
             position: google.maps.ControlPosition.RIGHT_TOP
         }
-    });
+    };
+
+    map =  new google.maps.Map( document.getElementById('map'), mapOptions);
 
     // zoom out for mobile screens, zoom in for larger screens
     // recenter the map when resizing the window
@@ -153,12 +106,9 @@ function initMap() {
         calculateCenter();
     });
 
-    addMarkers();
 };
 
 
-console.log("length of markers: " + markers.length);
-console.log("length of markers: " + infoWindows.length);
 
 
 
